cd2eaebcda38169660498bb038f54ee3ded1ee49
Bobby Pinard <bobby.pinard@gmail.com>
2021-10-07 19:46:35 -0500

    Added the rest of the ticker symbols

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,1 @@
-const d = new Date();
-const hour = d.getHours();
-const minute = d.getMinutes();
-
-document.getElementById("hour").innerHTML = hour;
-document.getElementById("minute").innerHTML = minute;
+document.searchform.search.focus();
