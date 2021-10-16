548f2a7366fcb210866ea8af93b66fd81f6e7c46
bobby Pinard <bobby.pinard@gmail.com>
2021-10-16 18:52:54 -0500

    Combined time and date into one clock widget.

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,11 +1,17 @@
 // Initial display of time
 const today = new Date();
 const time = today.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'});
-document.getElementById("clock").innerHTML = time;
+const date = today.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+document.getElementById("time").innerHTML = time;
+document.getElementById("date").innerHTML = date;
 
 // Auto reload time
 setInterval(() => {
     const today = new Date();
     const time = today.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'});
-    document.getElementById("clock").innerHTML = time;
+    const date = today.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+
+    document.getElementById("time").innerHTML = time;
+    document.getElementById("date").innerHTML = date;
 }, 1000);
--- scripts/date.js
+++ scripts/date.js
@@ -1,11 +1,11 @@
 // Initial display of date
-const atoday = new Date();
-const date = atoday.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+const dateToday = new Date();
+const date = dateToday.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
 document.getElementById("date").innerHTML = date;
 
 // Auto reload date
 setInterval(() => {
-    const atoday = new Date();
-    const date = atoday.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    const dateToday = new Date();
+    const date = dateToday.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
     document.getElementById("date").innerHTML = date;
 }, 1000);
