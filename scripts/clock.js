5974fa525013851781cb9c05228bc509e12ab858
bobby Pinard <bobby.pinard@gmail.com>
2021-10-17 09:56:11 -0500

    Cleaned up clock.

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,7 +1,7 @@
 // Initial display of time
 const today = new Date();
-const time = today.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'});
-const date = today.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+const time = today.toLocaleTimeString('en-GB', {hour: '2-digit', minute:'2-digit'});
+const date = today.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
 
 document.getElementById("time").innerHTML = time;
 document.getElementById("date").innerHTML = date;
@@ -9,8 +9,8 @@ document.getElementById("date").innerHTML = date;
 // Auto reload time
 setInterval(() => {
     const today = new Date();
-    const time = today.toLocaleTimeString('en-US', {hour: '2-digit', minute:'2-digit'});
-    const date = today.toLocaleTimeString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+    const time = today.toLocaleTimeString('en-GB', { hour: 'false', minute:'false' });
+    const date = today.toLocaleDateString('en-US', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' });
 
     document.getElementById("time").innerHTML = time;
     document.getElementById("date").innerHTML = date;
